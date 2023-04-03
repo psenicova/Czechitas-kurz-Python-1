f882c3ff7ad0ef8e54743bfab4ada81b75236d75
Jaroslava Pšenicová <psenicova.jaroslava@gmail.com>
2023-04-03 08:15:10 +0200

    Úkol č.1 - bonus

---
+++ ukol_1-bonus.py
@@ -0,0 +1,20 @@
+celeJmeno = input("Zadej jméno a příjmení: ")
+jmeno, prijmeni = celeJmeno.split(" ")
+
+#všechna písmena velká (vypíše např. JANA MALÁ)
+print(f"{jmeno.upper()} {prijmeni.upper()}")
+
+#všechna písmena malá (vypíše např. jana malá)
+print(f"{jmeno.lower()} {prijmeni.lower()}")
+
+#standardní varianta - první písmeno velké, další malá (vypíše např. Jana Malá)
+print(f"{jmeno.strip()} {prijmeni.strip()}")
+
+#iniciály (vypíše např. J. M.)
+print(jmeno[0] + ". " + prijmeni[0] + ".")
+
+#křestní jméno zkrácené na první písmeno a příjmení, pokud je křestní jméno delší než 5 znaků. Jinak vypíše standardní variantu, tj. první písmeno velké, další malá (u vstupu Jarmila Malá by došlo ke zkrácení křestního jména, zatímco u vstupu Jana Malá nikoliv)
+if len(jmeno)>5:
+    print(jmeno[0] + ". " + prijmeni)
+else:
+    print(celeJmeno)
