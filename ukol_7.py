92af44a1e1b165330c12368e3044ef5d58812862
Jaroslava Pšenicová <psenicova.jaroslava@gmail.com>
2023-05-10 16:53:46 +0200

    Úkol č. 7

---
+++ ukol_7.py
@@ -0,0 +1,32 @@
+class Auto:
+    def __init__(self, registracni_znacka, typ_vozidla, najete_km):
+        self.registracni_znacka = registracni_znacka
+        self.typ_vozidla = typ_vozidla
+        self.najete_km = najete_km
+        self.dostupne = True
+
+    def pujc_auto(self):
+        if self.dostupne:
+            self.dostupne = False
+            return "Potvrzuji zapůjčení vozidla"
+        else:
+            return "Vozidlo není k dispozici"
+    
+    def get_info(self):
+        return f"Poptávané vozidlo má SPZ {self.registracni_znacka} a jedná se o {self.typ_vozidla}."
+
+auto1 = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
+auto2 = Auto("1P3 4747", "Škoda Octavia", 41253)
+
+vozidlo = input("Jaké vozidlo si přejete půjčit?")
+
+if vozidlo == "Peugeot":
+    print(auto1.get_info())
+    print(auto1.pujc_auto())
+    print(auto1.pujc_auto())
+elif vozidlo == "Škoda":
+    print(auto2.get_info())
+    print(auto2.pujc_auto())
+    print(auto2.pujc_auto())
+else:
+    print("Požadované auto nemáme k dispozici")
