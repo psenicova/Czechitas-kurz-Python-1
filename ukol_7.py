2077f3a924d0134f31728dde09ea67a7502acd8e
Jaroslava Pšenicová <psenicova.jaroslava@gmail.com>
2023-06-06 18:23:38 +0200

    Úkol č. 7

--- ukol_7.py
+++ ukol_7.py
@@ -15,6 +15,15 @@ class Auto:
     def get_info(self):
         return f"Poptávané vozidlo má SPZ {self.registracni_znacka} a jedná se o {self.typ_vozidla}."
 
+    def vrat_auto(self, stav_tachometru, dny_vypujcky):
+        self.stav_tachometru = stav_tachometru
+        self.dny_vypujcky = dny_vypujcky
+        if not self.dostupne:
+            self.dostupne = True
+
+
+
+
 auto1 = Auto("4A2 3020", "Peugeot 403 Cabrio", 47534)
 auto2 = Auto("1P3 4747", "Škoda Octavia", 41253)
 
@@ -23,6 +32,8 @@ vozidlo = input("Jaké vozidlo si přejete půjčit?")
 if vozidlo == "Peugeot":
     print(auto1.get_info())
     print(auto1.pujc_auto())
+    print(auto1.pujc_auto())
+    print(auto1.vrat_auto())
     print(auto1.pujc_auto())
 elif vozidlo == "Škoda":
     print(auto2.get_info())
